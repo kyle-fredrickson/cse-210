dcb93739bb92dca36ee041df5f9724bbbfad70bd
Kyle Fredrickson <kyfredri@ucsc.edu>
2021-01-14 14:26:59 -0800

    Better precedence

--- language.hs
+++ language.hs
@@ -6,28 +6,46 @@ import System.IO
 import Text.ParserCombinators.ReadP
 
 data Expr = IntExpr {n :: Integer}
-          | SumExpr {l :: Expr, r :: Expr}
+          | AddExpr {l :: Expr, r :: Expr}
           | MulExpr {l :: Expr, r :: Expr}
           | ExpExpr {l :: Expr, r :: Expr}
 
+-- Eval --
 eval :: Expr -> Integer
 eval (IntExpr n) = n
-eval (SumExpr l r) = eval l + eval r
+eval (AddExpr l r) = eval l + eval r
 eval (MulExpr l r) = eval l * eval r
 eval (ExpExpr b e) = eval b ^ eval e
 
+-- Parse to Expr --
+
+parse :: [Char] -> Expr
 parse s =
     let l = parseToList s
     in parseNum l
 
+parseNum :: [String] -> Expr
 parseNum [] = IntExpr 0
-parseNum (x:[]) = IntExpr (read x :: Integer)
-parseNum (x:rest) = parseOp (IntExpr (read x :: Integer)) rest
+parseNum [x] = IntExpr (read x :: Integer)
+parseNum (x:r) = parseOp (IntExpr (read x :: Integer)) r
 
+parseOp :: Expr -> [String] -> Expr
 parseOp left [] = left
-parseOp left ("+":rest) = SumExpr left (parseNum rest)
-parseOp left ("*":num:rest) = parseOp (MulExpr left (parseNum [num])) rest
-parseOp left ("^":num:rest) = parseOp (ExpExpr left (parseNum [num])) rest
+parseOp left ("+":r) = parseAdd left r
+parseOp left ("*":r) = parseMul left r
+parseOp left ("^":r) = parseExp left r
+
+parseAdd :: Expr -> [String] -> Expr
+parseAdd left r = AddExpr left (parseNum r)
+
+parseMul :: Expr -> [String] -> Expr
+parseMul left (num:"+":r) = parseAdd (MulExpr left (parseNum [num])) r
+parseMul left right = MulExpr left (parseNum right)
+
+parseExp :: Expr -> [String] -> Expr
+parseExp left (num:"+":r) = parseAdd (ExpExpr left (parseNum [num])) r
+parseExp left (num:"*":r) = parseMul (ExpExpr left (parseNum [num])) r
+parseExp left right = ExpExpr left (parseNum right)
 
 -- Parser Parts --
 
@@ -71,4 +89,3 @@ main :: IO ()
 main = do
     expr <- getLine
     putStrLn "aaaaaahhh!"
-    --(print . eval . parse . splitOneOf " *+()") (head args)
