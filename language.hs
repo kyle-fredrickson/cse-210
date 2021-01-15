22ff9c0e550ec3411ac86085e2cfb31d6dcf1115
Kyle Fredrickson <kyfredri@ucsc.edu>
2021-01-14 17:50:24 -0800

    Minor

--- language.hs
+++ language.hs
@@ -1,56 +1,51 @@
 import Control.Applicative ( Alternative((<|>)) )
 import Data.Char
-import Data.List.Split
-import System.Environment
-import System.IO
 import Text.ParserCombinators.ReadP
 
-data Expr = IntExpr {n :: Integer}
-          | AddExpr {l :: Expr, r :: Expr}
-          | MulExpr {l :: Expr, r :: Expr}
-          | ExpExpr {l :: Expr, r :: Expr}
-
+data AST = IntExpr {n :: Integer}
+         | AddExpr {l :: AST, r :: AST}
+         | MulExpr {l :: AST, r :: AST}
+         | ExpExpr {l :: AST, r :: AST}
 
 -- Eval --
-eval :: Expr -> Integer
+
+eval :: AST -> Integer
 eval (IntExpr n) = n
 eval (AddExpr l r) = eval l + eval r
 eval (MulExpr l r) = eval l * eval r
 eval (ExpExpr b e) = eval b ^ eval e
 
--- Parse to Expr --
+-- Parse to AST --
 
-parse :: [Char] -> Expr
-parse s =
-    let l:r = parseToList s
-    in parseOp (IntExpr (read l :: Integer)) r
+parse :: [Char] -> AST
+parse s = let l:r = parseToList s
+    in parseOp (parseNum l) r
 
-parseNum :: String -> Expr
+parseNum :: String -> AST
 parseNum num = IntExpr (read num :: Integer)
 
-parseOp :: Expr -> [String] -> Expr
+parseOp :: AST -> [String] -> AST
 parseOp left [] = left
 parseOp left ("+":r) = parseAdd left r
 parseOp left ("*":r) = parseMul left r
 parseOp left ("^":r) = parseExp left r
 
-parseAdd :: Expr -> [String] -> Expr
+parseAdd :: AST -> [String] -> AST
 parseAdd left (num1:"*":num2:r) = AddExpr left (parseOp (MulExpr (parseNum num1) (parseNum num2)) r)
 parseAdd left (num1:"^":num2:r) = AddExpr left (parseOp (ExpExpr (parseNum num1) (parseNum num2)) r)
 parseAdd left (num:r) = parseOp (AddExpr left (parseNum num)) r
 
-parseMul :: Expr -> [String] -> Expr
+parseMul :: AST -> [String] -> AST
 parseMul left (num1:"^":num2:r) = MulExpr left (parseOp (ExpExpr (parseNum num1) (parseNum num2)) r)
 parseMul left (num:r) = parseOp (MulExpr left (parseNum num)) r
 
-parseExp :: Expr -> [String] -> Expr
+parseExp :: AST -> [String] -> AST
 parseExp left (num:r) = parseOp (ExpExpr left (parseNum num)) r
 
 -- Parser Parts --
 
 parseToList :: [Char] -> [String]
-parseToList s =
-    let parsed_string = readP_to_S arithExpr s
+parseToList s = let parsed_string = readP_to_S arithExpr s
     in if null parsed_string || snd (last parsed_string) /= ""
        then []
        else let ((num1, rest), _) = last parsed_string
@@ -86,7 +81,7 @@ arithOp =
 
 -- Debugging Tools --
 
-exprToStr :: Expr -> [Char]
+exprToStr :: AST -> [Char]
 exprToStr (IntExpr n) = show n
 exprToStr (AddExpr l r) = "(+ " ++ exprToStr l ++ " " ++ exprToStr r ++ ")"
 exprToStr (MulExpr l r) = "(* " ++ exprToStr l ++ " " ++ exprToStr r ++ ")"
